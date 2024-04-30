e50f59e72379230e1871c366f7fdb99f80b11c04
zzamytikk <zzamytikk@gmail.com>
2024-04-30 13:20:53 +0300

    👌 bg

--- zMenu/1-0-0/ok.js
+++ zMenu/1-0-0/ok.js
@@ -8,7 +8,7 @@ var zMenu = {//Меню
   $:q => {//Запуск
     let O=zMenu;
     
-    $(O.id).on('click', () => O.X(O));
+    $(O.id+' button').on('click', () => O.X(O));
     
     if(O.f.w()){//Меню не помещается! Делаем мобильную версию
       $(O.id).addClass('zMenuX');
@@ -24,13 +24,54 @@ var zMenu = {//Меню
       if($(O.id + '>div').width() >= $(O.id).width()){
         return 1
       }
+    },
+    /*  zMenu.f.bg({//✫ Добавим задний фон
+        id: 'string/event',     //* $() куда добавим. (.append(..);)
+        i: 'string',            //* id Для удаления zMenu.f.bg('string');
+        f: 'function',          //* ()=>{} Сработает при нажатие на задний фон
+
+        c:{                     //* css Заднего фона
+          'z-index': 7, //undefined = 9
+          top: 40,
+          background: ''//Свой задний фон. //Стандартный: #0006
+        }
+      });
+
+      zMenu.f.bg('string');//† Закрыть задний фон
+    */
+    bg: q => {
+      if (q.id) {
+        let i = 'bg-' + q.i;
+
+        $(q.id).append($('<div id="' + i + '" class="Obg">').css(q.c || {}));
+        $('#'+i).click(q.f);
+      } else {//† Закрыть задний фон
+        $('#bg-' + q).css({ height: 0, opacity: 0 });//Анимация закрытия
+
+        setTimeout(() => {
+          $('#bg-' + q).remove();//Удаляем задний фон
+        }, 300);
+      }
     }
   },
   X: O => {//Открыть/Закрыть
   let d = $(O.id + '>div'),
     b=$(O.id + ' button'),
     x=d.css('right')=='0px';//1 = Закрываем
+
     console.debug('right: '+d.css('right'), d.css('right')=='0px'?'Закроем':'Откроем');
+    
+    O.f.bg(
+      x
+        ? 'zMenu'//† Закрыть задний фон
+        : {//✫ Добавим задний фон
+          id: 'body', //* $() куда добавим. (.append(..);)
+          i: 'zMenu', //* id Для удаления zMenu.f.bg('string');
+          f: () => {  //* ()=>{} Сработает при нажатие на задний фон
+            O.X(zMenu);//† Закрыть
+            console.debug('bg'); 
+          }
+    });
 
     b.removeClass('B-I'+(x?'x-X':'x-M'))
       .addClass('B-I'+(x?'x-M':'x-X'));
